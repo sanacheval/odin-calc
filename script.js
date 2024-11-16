d7df1fee5d11eecfb097323b68fe3325993f3f8c
sanacheval <sanachevall@gmail.com>
2024-11-16 19:23:01 +0500

    adds operate function to run diff functions

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ function test(){
     console.log(`${multiply(3, 8)}, the answer should be 24`);
     console.log(`${divide(20, 5)}, the answer should be 4`);
 }
+
+let num1=0;
+let num2=0;
+let operator=".";
+function operate(num1, operator, num2) {
+    if (operator==="+"){
+        return add(num1, num2);
+    }
+    else if(operator==="-"){
+        return subtract(num1, num2);
+    }
+    else if(operator==="x" || operator==="*"){
+        return multiply(num1, num2);
+    }
+    else if(operator==="/" || operator==="÷"){
+        return divide(num1, num2);
+    }
+    else{
+        //not an operator
+    }
+}
